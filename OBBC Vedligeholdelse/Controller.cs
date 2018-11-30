b73e24132cb7bc5bf1906d11d21e80420a41639e
Christian Salgård Svinding <chri30cm@edu.eal.dk>
2018-11-30 12:55:36 +0100

    OBBC Vedligeholdelse

---
+++ OBBC Vedligeholdelse/Controller.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBBC_Vedligeholdelse
+{
+    class Controller
+    {
+        public 
+    }
+}
---
+++ OBBC Vedligeholdelse/Menu.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBBC_Vedligeholdelse
+{
+    class Menu
+    {
+        public void Show()
+        {
+            bool running = true;
+            do
+            {
+                ShowMenu();
+                string choice = GetUserChoice();
+                switch (choice)
+                {
+                    case "0":
+                        running = false;
+                        break;
+                    case "1":
+                        Console.Clear();
+                        //CheckMachines(); --- skal laves
+                        break;
+                    //case "2":
+                    //    Console.Clear();
+                    //    ShowScore();
+                    //    break;
+                    default:
+                        Console.WriteLine("Ugyldigt valg.");
+                        Console.ReadLine();
+                        break;
+                }
+            } while (running);
+        }
+
+        private void ShowMenu()
+        {
+            Console.WriteLine("Dragons Lair");
+            Console.WriteLine();
+            Console.WriteLine("1. Check alle maskiner");
+            Console.WriteLine("");
+            Console.WriteLine("0. Exit");
+        }
+
+        private string GetUserChoice()
+        {
+            Console.WriteLine();
+            Console.Write("Indtast dit valg: ");
+            return Console.ReadLine();
+        }
+
+        public void CheckMachines()
+        {
+
+        }
+    }
+}
