123daaafe9a49b53b376bb345de6b183835f0e90
Christian Salgård Svinding <chri30cm@edu.eal.dk>
2019-01-18 12:38:51 +0100

    GodsAnswer

--- UnitTestProject1/GodsAnswer.cs
+++ UnitTestProject1/GodsAnswer.cs
@@ -14,6 +14,10 @@ namespace UnitTestProject1
         public void IsListWorking()
         {
             database.ReadOnlyAllErrorReports();
+            ErrorReport report1 = new ErrorReport(1, "a", "b", "c", "d");
+            ErrorReport report2 = new ErrorReport(2, "e", "f", "g", "h", "i");
+            reportFactory.AddReport(report1);
+            reportFactory.AddReport(report2);
 
             Assert.AreEqual(2, reportFactory.GetErrorReports().Count);
         }
@@ -23,7 +27,7 @@ namespace UnitTestProject1
         {
             database.ReadAndShowErrorReports();
 
-            Assert.AreEqual(2, reportFactory.GetErrorReports().Count);
+            Assert.AreEqual(20, reportFactory.GetErrorReports().Count);
         }
     }
 }
